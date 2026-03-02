b0dfad3b5b6ba64047f056694c84350b2fc5a1fe
Fireflare5 <canonsparks@icloud.com>
2026-03-02 17:56:05 -0600

    Update

--- Prime101.cpp
+++ Prime101.cpp
@@ -1,21 +1,93 @@
-#include <queue>
-#include <thread>
-#include <mutex>
-#include <functional>
-#include <condition_variable>
 #include <iostream>
-#include "ThreadPool.h"
+#include <thread>
 
 class Prime101
 {
 public:
-    Prime101() : n_threads(std::thread::hardware_concurrency()), pool(std::thread::hardware_concurrency()) {}
+    std::vector<long> primes;
+    Prime101(long n) : Sieve("auto") {
+        //timer();
+        primes = find_primes(n);
+    }
 
-    Prime101(size_t num_threads) : n_threads(num_threads), pool(num_threads) {}
+    Prime101(std::string sieve, long n) : Sieve(sieve) {
+        //timer();
+        primes = find_primes(n);
+    }
 
-    std::vector<int> find_primes(int target_prime_)
+    Prime101(long n, std::string sieve) : Sieve(sieve) {
+        //timer();
+        primes = find_primes(n);
+    }
+    ~Prime101()
     {
-        std::thread timer([this] {
+        done = true;
+        for(auto& time : clock) {
+            time.join();
+        }
+    }
+    std::vector<long> find_primes(long n) {
+        if(Sieve == "eratosthenes" || Sieve == "sieve of eratosthenes" || Sieve == "soe" || Sieve == "SoE" || Sieve == "SOE" || Sieve == "eratos") {
+            Soe(n);
+        } else if(Sieve == "Atkin" || Sieve == "sieve of atkin" || Sieve == "soa" || Sieve == "SoA" || Sieve == "SOA" || Sieve == "atkin") {
+            Soa(n);
+        } else if(Sieve == "auto" || Sieve == "Auto") {
+            if(n < 10000000000L) {
+                Soe(n);
+            } else if(n > 10000000000L) {
+                Soa(n);
+            }
+        }
+        for(long i = 2; i <= n; ++i) {
+                if(primes_[i]) {
+                    primes.emplace_back(i);
+                }
+            }
+        return primes;
+    }
+
+    void Soe(long n) {
+        primes_.assign(n + 1, 1);
+        for(long i = 2; i * i <= n; ++i) {
+            if(primes_[i]) {
+                for(long j = i * i; j <= n; j += i) {
+                    primes_[j] = 0;
+                }
+            }
+        }
+    }
+
+    void Soa(long n) {
+        primes_.assign(n + 1, 0);
+        if(n > 2) primes_[2] = 1;
+        if(n > 3) primes_[3] = 1;
+        for(long i = 1; i * i <= n; ++i) {
+            std::cout << i << "\n";
+            for(long j = 1; j * j <= n; ++j) {
+                long k = 4 * i * i + j * j;
+                if(k <= n && (k % 12 == 1 || k % 12 == 5)) {
+                    primes_[k] = (primes_[k] + 1) % 2;
+                }
+                k = 3 * i * i + j * j;
+                if(k <= n && k % 12 == 7) {
+                    primes_[k] = (primes_[k] + 1) % 2;
+                }
+                k = 3 * i * i - j * j;
+                if(i > j && k <= n && k % 12 == 11) {
+                    primes_[k] = (primes_[k] + 1) % 2;
+                }
+            }
+        }
+        for(long i = 5; i * i <= n; ++i) {
+            if(!primes_[i]) continue;
+            for(long j = i * i; j <= n; j += i * i) {
+                primes_[j] = 0;
+            }
+        }
+    }
+
+    void timer() {
+        clock.emplace_back([this] {
             auto start = std::chrono::high_resolution_clock::now();
             while (true) {
                 if(done) {
@@ -47,106 +119,25 @@ public:
                 }
             }
         });
-        while (primes_.size() < target_prime_) {
-            
-            if(primes_.back() < sqrt(target_prime_)) {
-                prime2 = primes_.back() * primes_.back();
-            } else {
-                prime2 = n + target_prime_;
-            }
-            
-            primes_queued_ = 0;
-            proceed = false;
-            synced = false;
-
-            while (n < prime2 && n_cache_.size() + primes_.size() < target_prime_) {
-                n += 2;
-                if(n % 3 == 0 || n % 5 ==0) {
-                    continue;
-                }
-
-                n_cache_.emplace(n);
-                primes_queued_++;
-
-                pool.enqueue([this, target_prime_] {
-                    bool is_prime = true;
-                    int np;
-                    {
-                        std::unique_lock<std::mutex> lock(mute_);
-                        cv_.wait(lock, [this] {
-                            return synced;
-                        });
-                        np = std::move(n_cache_.front());
-                        n_cache_.pop();
-                        for(auto& prime : primes_) {;
-                            if(prime == sqrt(np)) {
-                                is_prime = false;
-                                break;
-                            } else if(!(prime < sqrt(np))) {
-                                break;
-                            } else if(np % prime == 0) {
-                                is_prime = false;
-                                break;
-                            }
-                        }
-                        if(is_prime) {
-                            prime_cache_.emplace(std::move(np));
-                        }
-                        primes_queued_--;
-                        if(primes_queued_ == 0) {
-                            proceed = true;
-                            cv_.notify_one();
-                        }
-                    }
-                });
-                
-            }
-            {
-                std::unique_lock<std::mutex> lock(mute_);
-                synced = true;
-                cv_.notify_all();
-                cv_.wait(lock, [this] {
-                    return proceed;
-                });
-                int i = 0;
-                while(!prime_cache_.empty()) {
-                    if(prime_cache_.front() > primes_.back()) {
-                        primes_.emplace_back(prime_cache_.front());
-                        prime_cache_.pop();
-                    }else if(prime_cache_.front() < primes_[i]) {
-                        primes_.emplace(primes_.begin() + i, std::move(prime_cache_.front()));
-                        prime_cache_.pop();
-                    }
-                    i++;
-                }
-            }
-        }
-        done = true;
-        timer.join();
-        return primes_;
     }
 
 private:
-    int n = 5;
-    int prime2;
+    std::string Sieve;
+    std::vector<long> primes_;
+    std::vector<std::thread> clock;
     bool done = false;
-    ThreadPool pool;
-    std::mutex mute_;
-    int n_threads;
-    int primes_queued_ = 0;
-    bool synced;
-    bool proceed;
-    std::queue<int> n_cache_;
-    std::condition_variable cv_;
-    std::queue<int> prime_cache_;
-    std::vector<int> primes_ = {2,3,5};
 };
 
+inline std::ostream& operator<<(std::ostream& out, const Prime101& v) {
+    for(auto& prime : v.primes) {
+        out << prime << " ";
+    }
+    return out;
+}
+
 int main() {
-    int target;
-    Prime101 prime101;
-    std::cout << "Input target prime: ";
-    std::cin >> target;
-    std::vector<int> primes = prime101.find_primes(target);
-    std::cout << primes[target - 1] << std::endl;
+    int limit = 1000000000;
+    Prime101 prime(limit,"soa");
+    std::cout << prime << "\n";
+    return 0;
 }
